45231fe9ed56ddf6c3cf12688192d517334824bd
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-04-01 19:07:03 +0300

    Create maim.java

---
+++ maim.java
@@ -0,0 +1,8 @@
+import methods.StartProgram;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StartProgram.Start("input.txt");
+
+    }
