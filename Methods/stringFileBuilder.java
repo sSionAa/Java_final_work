7ad3a64cc946a5b7d59bcbedce8bb1c3ba72c014
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-04-01 19:18:48 +0300

    Create stringFileBuilder.java

---
+++ Methods/stringFileBuilder.java
@@ -0,0 +1,19 @@
+package Methods;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class stringFileBuilder {
+
+    public static void BuilderString(String path)  {
+        try {
+            String result = Files.readString(Paths.get(path));
+            String[] picnic = result.toLowerCase().replaceAll("\\s+"," ").split(" ");
+            WordCounter.WordsCounter(picnic);
+        }catch (IOException e){
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+}
