77d7bb20bf1fb9581e4d2c77a10fba62ed47e1c6
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-04-01 20:06:57 +0300

    Update CheckerLength.java

--- Methods/CheckerLength.java
+++ Methods/CheckerLength.java
@@ -1,6 +1,6 @@
 package Methods;
 
-public class CheckerLength {
+public class checkerLength {
     public static String CheckLen(String a, String b) {
         if (a.equals(b)) return a;
         if (a.length()>b.length()) {return  a;} else return b;
