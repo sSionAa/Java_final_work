742911308bbfeaac39604d873da46452f5614c3d
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-04-01 19:10:41 +0300

    Create wordCounter.java

---
+++ Methods/wordCounter.java
@@ -0,0 +1,29 @@
+package methods;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCounter {
+    public static void WordCounter(String[] picnic) {
+        String maxLength = picnic[0];
+        int countOfWords = 0;
+        Map<String,Integer> uniqPicnic = new HashMap<>();
+        for (String foods : picnic ) {
+            countOfWords+=1;
+            maxLength = CheckLength.CheckLen(maxLength,foods);
+            if (!uniqPicnic.containsKey(foods)){
+                uniqPicnic.put(foods,1);
+            }else {
+                uniqPicnic.put(foods, uniqPicnic.get(foods)+1);
+            }
+        }
+        System.out.printf("Самое длинное слово в файле = %s \n", maxLength);
+        System.out.printf("Количество слов в файле = %d \n", countOfWords);
+        System.out.println("Частота слов : ");
+        for (Map.Entry<String, Integer> stringIntegerEntry : uniqPicnic.entrySet()) {
+            System.out.println(stringIntegerEntry);
+        }
+
+
+    }
+}
